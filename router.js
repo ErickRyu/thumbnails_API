5de9222cdf1601e467387b84baa12bbb68d70bac
ErickRyu <rkggmdii@gmail.com>
2017-02-17 19:07:31 +0900

    Update: Make phantomjs run on node, Comibe /thumbnails and /thumbnails/images

--- router.js
+++ router.js
@@ -1,25 +1,12 @@
 var express = require('express');
 var app = express();
-var path = require('path')
-var childProcess = require('child_process')
-var phantomjs = require('phantomjs')
-var binPath = phantomjs.path
 var fs = require('fs')
+var async = require('async');
 
+var defaultTimeout = 8000;
 
-app.get('/thumbnails', function(req, res){
-	url = req.query.url;
-	res.send(url + " is saved as " + encodeURIComponent(url) + ".png");
-	var childArgs = [
-	path.join(__dirname, 'capture.js'),
-	url
-	]
-	childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-		// handle results
-		console.log(stdout);
-	})
-})
-app.get('/thumbnails/images', function(req, res){
+app.get('/thumbnails', function(req, res) {
+	
 	var url = req.query.url;
 	var mod = 0;
 	if(req.query.mod){
@@ -27,22 +14,47 @@ app.get('/thumbnails/images', function(req, res){
 	}
 	encodedURI = encodeURIComponent(url);
 	pathAndFile =  __dirname + '/' + encodedURI + '.png';
-	if(mod == 0){
-		res.sendFile(pathAndFile);
-	}else if(mod == 1){
+	
+	// Have to make it syncronously
+	save(url, pathAndFile);
 
-		fs.readFile(pathAndFile, function(err, original_data){
+
+	setTimeout(function(){
+		if(mod == 0){
+			res.sendFile(pathAndFile);
+		}else if(mod == 1){
+			fs.readFile(pathAndFile, function(err, original_data){
 			// fs.writeFile('image_orig.jpg', original_data, function(err) {});
 			var base64Image = original_data.toString('base64');
 			// var decodedImage = new Buffer(base64Image, 'base64');
 			// fs.writeFile('image_decoded.jpg', decodedImage, function(err) {});
-
 			res.send(base64Image);
 		});
-	}
-})
+		}		
+	}, 9000);
+	
+	
+	
+});
 
+var save = function(url, pathAndFile){
+	var phantom = require('phantom');
+	phantom.create().then(function(ph) {
+		ph.createPage().then(function(page) {
+			page.open(url).then(function(status) {
+				console.log(status);
+				page.property('content').then(function(content) {
+					page.property('viewportSize',{ width: 1024, height: 740 });
+					page.property('clipRect',{ top: 0, left: 0, width: 700, height: 400 });
+					page.render(pathAndFile);
+					page.close();
+					ph.exit();
 
+				});
+			});
+		});
+	});
+}
 var server = app.listen(8081, function () {
 	var host = server.address().address
 	var port = server.address().port
