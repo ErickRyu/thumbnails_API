f48e0756f795c570efac73e18ff6031da938c9ef
ErickRyu <rkggmdii@gmail.com>
2017-02-19 18:50:11 +0900

    Update: Made send.red synchronously

--- router.js
+++ router.js
@@ -16,24 +16,42 @@ app.get('/thumbnails', function(req, res) {
 	pathAndFile =  __dirname + '/' + encodedURI + '.png';
 	
 	// Have to make it syncronously
-	save(url, pathAndFile);
-
-
-	setTimeout(function(){
-		if(mod == 0){
-			res.sendFile(pathAndFile);
-		}else if(mod == 1){
-			fs.readFile(pathAndFile, function(err, original_data){
-			// fs.writeFile('image_orig.jpg', original_data, function(err) {});
-			var base64Image = original_data.toString('base64');
-			// var decodedImage = new Buffer(base64Image, 'base64');
-			// fs.writeFile('image_decoded.jpg', decodedImage, function(err) {});
-			res.send(base64Image);
+	
+	var phantom = require('phantom');
+	phantom.create().then(function(ph) {
+		var start = (new Date).getTime();
+		ph.createPage().then(function(page) {
+			page.open(url).then(function(status) {
+				console.log(status);
+				page.property('content').then(function(content) {
+					page.property('viewportSize',{ width: 1024, height: 450 });
+					page.property('clipRect',{ top: 0, left: 0, width: 700, height: 450 });
+					page.render(pathAndFile).then(function(err,stdout){
+						if(mod == 0){
+							res.sendFile(pathAndFile);
+						}else if(mod == 1){
+							fs.readFile(pathAndFile, function(err, original_data){
+								// fs.writeFile('image_orig.jpg', original_data, function(err) {});
+								var base64Image = original_data.toString('base64');
+								// var decodedImage = new Buffer(base64Image, 'base64');
+								// fs.writeFile('image_decoded.jpg', decodedImage, function(err) {});
+								res.send(base64Image);
+							});
+						}
+						var end = (new Date).getTime();
+						console.log((end-start)/1000. + " elapsed");
+					});
+					page.close();
+					ph.exit();
+					
+				});
+			});
 		});
-		}		
-	}, 9000);
+	});
+
+	// var promise = save(url, pathAndFile);
 	
-	
+
 	
 });
 
@@ -49,7 +67,7 @@ var save = function(url, pathAndFile){
 					page.render(pathAndFile);
 					page.close();
 					ph.exit();
-
+					res.sendFile(pathAndFile);
 				});
 			});
 		});
