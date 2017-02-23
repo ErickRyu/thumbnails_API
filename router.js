4060f4b3a5c8a4ab7840a5443f750b60174dea64
ErickRyu <rkggmdii@gmail.com>
2017-02-23 17:30:26 +0900

    Update: Add error handling

--- router.js
+++ router.js
@@ -1,28 +1,45 @@
 const screenshot = require('screenshot-stream');
 var express = require('express');
 var app = express();
-var width = 1024;
-var height = 468;
 
 
 app.get('/thumbnails', function(req, res) {
 	var start = (new Date).getTime();
 
-
 	var url = req.query.url;
+
 	var mod = 'png';
-	if(req.query.mod)
+	var width = 1024;
+	var height = 468;
+
+	// Query check
+	if(req.query.mod){
 		mod = req.query.mod;
-	if(req.query.width)
+	}
+	if(req.query.width){
 		width = req.query.width;
-	if(req.query.height)
-		height = req.query.height;
+		if(isNaN(width)){
+			res.status(400).send("Width must be the nubmer");
+			res.end();
+		}
+	}
+	if(req.query.height){
+		height = req.query.height;		
+		if(isNaN(height)){
+			res.status(400).send("Height must be the nubmer");
+			res.end();
+		}
+	}
 
 	const stream = screenshot(url, width + 'x' + height, {crop: true});
 	
+	// stream error
 	stream.on('error', function(err){
-		console.log('somthing wrong');
-		res.status(400).send(err);
+		console.log('=========================================')
+		console.log('Stream error');
+		console.log(err.stack);
+		console.log('=========================================')
+		res.status(400).send(err.message);
 	})
 
 	if(mod == 'png'){
@@ -35,7 +52,7 @@ app.get('/thumbnails', function(req, res) {
 		})
 	}else {
 		res.status(400);
-		res.send('wrong mod');
+		res.send("Wrong mod!\nAvailable mod is 'base64' and 'png'");
 	}
 	
 	stream.on('end', function(){
@@ -43,10 +60,18 @@ app.get('/thumbnails', function(req, res) {
 		console.log((end-start)/1000. + " Elapsed");
 		res.end();
 	})
-	stream.on('error', function(exc) {
-		console.error("ignoring exception " + exc);
-	});
 });
+
+// 404 Error Handling
+app.use(function(req, res, next){
+	res.status(404).send("Page Not found");
+})
+
+app.use(function(err, req, res, next) {
+	console.log(err.stack);
+	res.status(500).send(err.message);
+});
+	
 
 var server = app.listen(8081, function () {
 	var host = server.address().address
