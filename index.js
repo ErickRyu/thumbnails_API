de82802680ebb594dd5550672eae986cbc5d939d
ErickRyu <rkggmdii@gmail.com>
2017-03-03 12:31:39 +0900

    Create: Create resize api

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
 const screenshot = require('screenshot-stream');
 var express = require('express');
 var morgan = require('morgan');
+var fs = require('fs');
+var multer = require('multer')
+var lwip = require('lwip');
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'my-uploads/')
+	},
+	filename: function (req, file, cb) {
+		cb(null, Date.now() + file.originalname)
+  }
+})
+var upload = multer({storage: storage})
 var app = express();
 module.exports = app;
 morgan.token('date', function() {
     var p = new Date().toString().replace(/[A-Z]{3}\+/,'+').split(/ /);
     return( p[2]+'/'+p[1]+'/'+p[3]+':'+p[4]+' '+p[5] );
 });
-var fs = require('fs');
 app.use(morgan('combined', {
 	stream: fs.createWriteStream('app.log', {'flags' :'w'})
 }));
+
+app.get('/resize', function(req, res){
+	fs.readFile('upload.html', function(err, pgRes) {
+		if(err) throw err;
+		else
+			res.write(pgRes);
+	});
+});
+app.post('/resize', upload.single('image'), function(req,res,next){
+	console.log(req.body);
+	body = req.body;
+	width = parseInt(body.width);
+	height = parseInt(body.height);
+	mod = body.mod;
+	console.log(width);
+	console.log(height);
+	console.log(mod);
+	console.log(req.file);
+	filename = req.file.filename;
+	path = __dirname + "/my-uploads/" + filename;
+	console.log(path);
+  lwip.open(path, function(err, image){
+		image.resize(width, height, function(err, image) {
+			image.writeFile(path, function(err){
+				res.sendFile(path);
+			});
+		});
+	});
+});
 
 app.get('/thumbnails', function(req, res) {
 	var url = req.query.url;
@@ -37,8 +77,8 @@ app.get('/thumbnails', function(req, res) {
 			res.end();
 		}
 	}
-
-	const stream = screenshot(url, width + 'x' + height, {crop: true});
+	var size = width+'x'+height;
+	const stream = screenshot(url, size, {crop: true});
 	
 	// stream error
 	stream.on('error', function(err){
@@ -78,7 +118,7 @@ app.use(function(err, req, res, next) {
 });
 	
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT||8081, function () {
 	var host = server.address().address
 	var port = server.address().port
 
